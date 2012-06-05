8c9a52af706b42ac2d20f269c000a4d27a520942
Starbuck Johnson <Starbuck Johnson@tweetlebeetles>
2012-06-05 14:32:57 -0500

    Added testing for colors. Need to update both methods though.

---
+++ src/me/modwizcode/plugin/colorCom/PluginTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.modwizcode.plugin.colorCom;
+
+/**
+ *
+ * @author Starbuck Johnson
+ */
+public class PluginTest {
+    public static void main(String args[]) {
+        ColorCom plugin = new ColorCom();
+        System.out.println(plugin.onChat("&&Hello"));
+    }
+    
+}
